8eee67b311d68f59bfe71309d1e0688d37020ee3
marco-lancini <info@marcolancini.it>
2023-03-30 22:01:15 +0100

    cleanup

--- terraform/aws-ecs-zero-trust/terraform/flaskapp/resources.tf
+++ terraform/aws-ecs-zero-trust/terraform/flaskapp/resources.tf
@@ -30,70 +30,15 @@ module "flask_cluster" {
 # ==============================================================================
 # ECR
 # ==============================================================================
-#
 # Cloudflared
-#
 resource "aws_ecr_repository" "cloudflared" {
-  #checkov:skip=CKV_AWS_33:Ensure ECR image scanning on push is enabled
-  #checkov:skip=CKV_AWS_51:Ensure ECR Image Tags are immutable
   name                 = var.ecr_cloudflared_name
   image_tag_mutability = "MUTABLE"
 }
 
-resource "aws_ecr_lifecycle_policy" "cloudflared" {
-  repository = aws_ecr_repository.cloudflared.name
 
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Expire untagged images older than 1 days",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "sinceImagePushed",
-                "countUnit": "days",
-                "countNumber": 1
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
-
-#
 # Flask Server
-#
 resource "aws_ecr_repository" "flask" {
-  #checkov:skip=CKV_AWS_33:Ensure ECR image scanning on push is enabled
-  #checkov:skip=CKV_AWS_51:Ensure ECR Image Tags are immutable
   name                 = var.ecr_flask_name
   image_tag_mutability = "MUTABLE"
-}
-
-resource "aws_ecr_lifecycle_policy" "flask" {
-  repository = aws_ecr_repository.flask.name
-
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Expire untagged images older than 1 days",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "sinceImagePushed",
-                "countUnit": "days",
-                "countNumber": 1
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
 }
